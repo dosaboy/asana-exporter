e6c0ec3759d0373ff04b2248b498347a289e37ee
Edward Hope-Morley <edward.hope-morley@canonical.com>
2023-01-26 11:57:50 +0000

    Use class based resources

--- asana_to_jira/client.py
+++ asana_to_jira/client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import abc
 import argparse
 import os
 import re
@@ -21,6 +22,20 @@ def with_lock(f):
     return with_lock_inner
 
 
+def required(opts):
+    def _required(f):
+        def _inner_required(self, *args, **kwargs):
+            if not all([getattr(self, o) for o in opts]):
+                opts_fmtd = ['--{}'.format(o) for o in opts]
+                msg = ("one or more of the following required options have "
+                       "not been provided: {}".format(', '.join(opts_fmtd)))
+                raise Exception(msg)
+            return f(self, *args, **kwargs)
+
+        return _inner_required
+    return _required
+
+
 class Logger(object):
 
     def debug(self, msg):
@@ -39,6 +54,199 @@ class Logger(object):
 LOG = Logger()
 
 
+class AsanaResourceBase(abc.ABC):
+
+    @abc.abstractmethod
+    def _from_api(self):
+        """ Fetch resources from the API. """
+
+    @abc.abstractmethod
+    def _from_local(self):
+        """ Fetch resources from the local cache/export. """
+
+    @with_lock
+    def get(self, update_from_api=True, prefer_cache=True):
+        if prefer_cache:
+            objs = self._from_local()
+        else:
+            objs = []
+
+        if not objs:
+            if update_from_api:
+                objs = self._from_api()
+
+        if not objs and not prefer_cache:
+            objs = self._from_local()
+
+        return objs
+
+
+class AsanaProjects(AsanaResourceBase):
+
+    def __init__(self, client, workspace, team,
+                 projects_include_filter, project_exclude_filter,
+                 projects_json):
+        self.client = client
+        self.workspace = workspace
+        self.projects_include_filter = projects_include_filter
+        self.project_exclude_filter = project_exclude_filter
+        self.projects_json = projects_json
+        self.team = team
+
+    def _from_local(self):
+        projects = []
+        LOG.info("fetching projects for team '{}' from cache".
+                 format(self.team['name']))
+        if not os.path.exists(self.projects_json):
+            LOG.info("no cached projects to list")
+            return projects
+
+        with open(self.projects_json) as fd:
+            projects = json.loads(fd.read())
+
+        return projects
+
+    def _from_api(self):
+        LOG.info("fetching projects for team '{}' from api".
+                 format(self.team['name']))
+        total = 0
+        ignored = []
+        projects = []
+        for p in self.client.projects.find_by_workspace(
+                                                  workspace=self.workspace,
+                                                  team=self.team['gid']):
+            total += 1
+            if self.projects_include_filter:
+                if not re.search(self.projects_include_filter, p['name']):
+                    ignored.append(p['name'])
+                    continue
+
+            if self.project_exclude_filter:
+                if re.search(self.project_exclude_filter, p['name']):
+                    ignored.append(p['name'])
+                    continue
+
+            projects.append(p)
+
+        if ignored:
+            LOG.info("ignoring projects:\n  {}".
+                     format('\n  '.join(ignored)))
+
+        LOG.info("saving {}/{} projects".format(len(projects), total))
+        if projects:
+            LOG.info("updating project cache")
+            """
+            if os.path.exists(self.projects_json):
+                with open(self.projects_json) as fd:
+                    _projects = json.loads(fd.read())
+
+                for p in _projects:
+                    if p not in projects:
+                        projects.append(p)
+            """
+
+            with open(self.projects_json, 'w') as fd:
+                fd.write(json.dumps(projects))
+
+        LOG.info("fetched {} projects for team '{}'".
+                 format(len(projects), self.team['name']))
+        return projects
+
+
+class AsanaProjectTasks(AsanaResourceBase):
+
+    def __init__(self, client, project, projects_dir):
+        self.client = client
+        self.project = project
+        self.root_path = os.path.join(projects_dir, project['gid'])
+
+    def _from_local(self):
+        tasks = []
+        if not os.path.exists(os.path.join(self.root_path, 'tasks.json')):
+            return tasks
+
+        LOG.info("fetching tasks for project '{}' (gid={}) from cache".
+                 format(self.project['name'], self.project['gid']))
+        with open(os.path.join(self.root_path, 'tasks.json')) as fd:
+            tasks = json.loads(fd.read())
+
+        LOG.info("fetched {} tasks".format(len(tasks)))
+        return tasks
+
+    def _from_api(self):
+        tasks = []
+        LOG.info("fetching tasks for project '{}' (gid={}) from api".
+                 format(self.project['name'], self.project['gid']))
+        path = os.path.join(self.root_path, 'tasks')
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+        tasks = []
+        for t in self.client.projects.tasks(self.project['gid']):
+            tasks.append(t)
+
+        for t in tasks:
+            task_path = os.path.join(path, t['gid'])
+            if os.path.exists(task_path):
+                continue
+
+            if not os.path.isdir(task_path):
+                os.makedirs(task_path)
+
+        with open(os.path.join(self.root_path, 'tasks.json'), 'w') as fd:
+            fd.write(json.dumps(tasks))
+
+        LOG.info("saved {} tasks".format(len(tasks)))
+        LOG.info("fetched {} tasks".format(len(tasks)))
+        return tasks
+
+
+class AsanaProjectTaskStories(AsanaResourceBase):
+
+    def __init__(self, client, project, projects_dir, task):
+        self.client = client
+        self.project = project
+        self.task = task
+        self.root_path = os.path.join(projects_dir, project['gid'],
+                                      'tasks', task['gid'])
+
+    def _from_local(self):
+        stories = []
+        p_gid = self.project['gid'].strip()
+        t_name = self.task['name'].strip()
+        if os.path.exists(os.path.join(self.root_path, 'stories.json')):
+            LOG.info("fetching stories for task '{}' (project={}) from cache".
+                     format(t_name, p_gid))
+            with open(os.path.join(self.root_path, 'stories.json')) as fd:
+                stories = json.loads(fd.read())
+
+        return stories
+
+    def _from_api(self):
+        p_gid = self.project['gid'].strip()
+        t_name = self.task['name'].strip()
+        LOG.info("fetching stories for task='{}' (project gid={}) from "
+                 "api".format(t_name, p_gid))
+        path = os.path.join(self.root_path, 'stories')
+        if not os.path.isdir(path):
+            os.makedirs(path)
+
+        stories = []
+        for s in self.client.stories.find_by_task(self.task['gid']):
+            stories.append(s)
+
+        for s in stories:
+            with open(os.path.join(path, s['gid']), 'w') as fd:
+                fd.write(json.dumps(s))
+
+        with open(os.path.join(self.root_path, 'stories.json'),
+                  'w') as fd:
+            fd.write(json.dumps(stories))
+
+        LOG.info("saved {} stories".format(len(stories)))
+        return stories
+
+
 class AsanaExtractor(object):
 
     def __init__(self, token, workspace, teamname, export_path,
@@ -54,6 +262,7 @@ class AsanaExtractor(object):
             os.makedirs(self.export_path)
 
     @cached_property
+    @required(['token', '_workspace', 'teamname'])
     def client(self):
         if not self.token:
             raise Exception("api oauth token must be provided with --token")
@@ -133,58 +342,11 @@ class AsanaExtractor(object):
         Since the api can be slow, it is recommended to use the available
         project filters.
         """
-        projects = []
-        if not update_from_api:
-            if not os.path.exists(self.projects_json):
-                LOG.info("no cached projects to list")
-                return projects
-
-            LOG.info("fetching projects from cache")
-            with open(self.projects_json) as fd:
-                projects = json.loads(fd.read())
-        else:
-            LOG.info("fetching projects from api")
-            total = 0
-            ignored = []
-            for p in self.client.projects.find_by_workspace(
-                                                      workspace=self.workspace,
-                                                      team=self.team['gid']):
-                total += 1
-                if self.projects_include_filter:
-                    if not re.search(self.projects_include_filter, p['name']):
-                        ignored.append(p['name'])
-                        continue
-
-                if self.project_exclude_filter:
-                    if re.search(self.project_exclude_filter, p['name']):
-                        ignored.append(p['name'])
-                        continue
-
-                projects.append(p)
-
-            if ignored:
-                LOG.info("ignoring projects:\n  {}".
-                         format('\n  '.join(ignored)))
-
-            LOG.info("saving {}/{} projects".format(len(projects), total))
-            if projects:
-                LOG.info("updating project cache")
-                """
-                if os.path.exists(self.projects_json):
-                    with open(self.projects_json) as fd:
-                        _projects = json.loads(fd.read())
-
-                    for p in _projects:
-                        if p not in projects:
-                            projects.append(p)
-                """
-
-                with open(self.projects_json, 'w') as fd:
-                    fd.write(json.dumps(projects))
-
-        LOG.info("fetched {} projects for team '{}'".
-                 format(len(projects), self.team['name']))
-        return projects
+        return AsanaProjects(self.client, self.workspace, self.team,
+                             self.projects_include_filter,
+                             self.project_exclude_filter,
+                             self.projects_json).get(update_from_api,
+                                                     prefer_cache=False)
 
     def get_project_templates(self):
         root_path = path = os.path.join(self.projects_dir)
@@ -210,76 +372,19 @@ class AsanaExtractor(object):
         LOG.info("fetched {} templates".format(len(templates)))
         return templates
 
-    @with_lock
     def get_project_tasks(self, project, update_from_api=True):
-        tasks = []
-        root_path = path = os.path.join(self.projects_dir, project['gid'])
-        if os.path.exists(os.path.join(root_path, 'tasks.json')):
-            LOG.info("fetching tasks for project '{}' from cache".
-                     format(project['name']))
-            with open(os.path.join(root_path, 'tasks.json')) as fd:
-                tasks = json.loads(fd.read())
-        elif update_from_api:
-            LOG.info("fetching tasks for project '{}' from api".
-                     format(project['name']))
-            path = os.path.join(root_path, 'tasks')
-            if not os.path.isdir(path):
-                os.makedirs(path)
+        """
+        @param update_from_api: allow fetching from the API.
+        """
+        return AsanaProjectTasks(self.client, project,
+                                 self.projects_dir).get(update_from_api)
 
-            tasks = []
-            for t in self.client.projects.tasks(project['gid']):
-                tasks.append(t)
-
-            for t in tasks:
-                task_path = os.path.join(path, t['gid'])
-                if os.path.exists(task_path):
-                    continue
-
-                if not os.path.isdir(task_path):
-                    os.makedirs(task_path)
-
-            with open(os.path.join(root_path, 'tasks.json'), 'w') as fd:
-                fd.write(json.dumps(tasks))
-
-            LOG.info("saved {} tasks".format(len(tasks)))
-
-        LOG.info("fetched {} tasks".format(len(tasks)))
-        return tasks
-
-    @with_lock
-    def get_task_stories(self, project, task):
-        root_path = os.path.join(self.projects_dir, project['gid'], 'tasks',
-                                 task['gid'])
-        p_name = project['name'].strip()
-        t_name = task['name'].strip()
-        if os.path.exists(os.path.join(root_path, 'stories.json')):
-            LOG.info("fetching stories for project '{}' task '{}' from cache".
-                     format(p_name, t_name))
-            with open(os.path.join(root_path, 'stories.json')) as fd:
-                stories = json.loads(fd.read())
-        else:
-            LOG.info("fetching stories for project='{}' task='{}' from api".
-                     format(p_name, t_name))
-            path = os.path.join(root_path, 'stories')
-            if not os.path.isdir(path):
-                os.makedirs(path)
-
-            stories = []
-            for s in self.client.stories.find_by_task(task['gid']):
-                stories.append(s)
-
-            for s in stories:
-                with open(os.path.join(path, s['gid']), 'w') as fd:
-                    fd.write(json.dumps(s))
-
-            with open(os.path.join(root_path, 'stories.json'),
-                      'w') as fd:
-                fd.write(json.dumps(stories))
-
-            LOG.info("saved {} stories".format(len(stories)))
-
-        LOG.info("fetched {} stories".format(len(stories)))
-        return stories
+    def get_task_stories(self, project, task, update_from_api=True):
+        """
+        @param update_from_api: allow fetching from the API.
+        """
+        return AsanaProjectTaskStories(self.client, project, self.projects_dir,
+                                       task).get(update_from_api)
 
     def run_job(self, project):
         for t in self.get_project_tasks(project):
@@ -383,11 +488,6 @@ def main():
 
                 break
     else:
-        if not all([args.token, args.workspace, args.team]):
-            msg = ("one or more of the following required options have not "
-                   "been provided: --token, --workspace, --team")
-            raise Exception(msg)
-
         ae.run()
 
 
